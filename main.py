31d943c9ce980c0bc9cb3040f5f5ef56113bca25
ponyta <chunli.developer@gmail.com>
2014-03-29 01:00:53 -0400

    make post request work

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect
+from flask import url_for
 from flask.ext.socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yext1234'
+app.config.update(
+    SECRET_KEY='yext1234',
+    DEBUG=True
+)
 socketio = SocketIO(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'GET':
         return render_template('index.html')
-    else:
+    elif request.method == 'POST':
         # process room and stuff here
-        return 'asdf'
+        room = request.form['room']
+        return redirect(url_for('room', room_name=room))
+
+@app.route('/room/<room_name>')
+def room(room_name):
+    return room_name
 
 if __name__ == '__main__':
-    socketio.run(app)
+    if app.debug:
+        app.run(debug=True)
+    else:
+        socketio.run(app)

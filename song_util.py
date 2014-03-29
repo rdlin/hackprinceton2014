08ffe2cf67d4530d7c88c888fa118d3b310d63b2
rdlin <rdlin@gmail.com>
2014-03-29 04:13:49 -0400

    Adding search by artist

--- song_util.py
+++ song_util.py
@@ -23,11 +23,22 @@ def get_item_string_for_top_chart_tracks(item=None):
             "consult http://www.rdio.com/developers/docs/web-service/types/track/ for proper types")
     return "<br>".join(get_top_chart_tracks_items(item, top_chart_tracks))
 
-#getting lyrics and a bunch of other information for a track
+#getting lyrics and a bunch of other information for a track search
 @app.route("/search/<track_name>/info")
 def get_info_for_track_name_search(track_name=None):
     displayString = ""
     for track in search_for_tracks(track_name):
+        displayString+="Name: " + track["name"]+"<br>"
+        displayString+="Artist: " + track["artist"]+"<br>"
+        displayString+="Lyrics: " + get_lyrics_for_track_name(name=track["name"], artist=track["artist"])+"<br>"
+        displayString+="<br><br>"
+    return displayString
+
+#getting lyrics and a bunch of other information for a track search by artist
+@app.route("/search/artist/<artist_name>/info")
+def get_info_for_track_name_by_artist_search(artist_name=None):
+    displayString = ""
+    for track in search_for_tracks(artist_name):
         displayString+="Name: " + track["name"]+"<br>"
         displayString+="Artist: " + track["artist"]+"<br>"
         displayString+="Lyrics: " + get_lyrics_for_track_name(name=track["name"], artist=track["artist"])+"<br>"
@@ -58,6 +69,15 @@ def get_top_chart_tracks(count="20"):
 def search_for_tracks(search_query):
     rdio = Rdio(("ucvxaju3gq3natuvp9w6uxrv", "vwnQkPkDhM"))
     search_results = rdio.call("search", {"query": search_query, "types": "Track"})
+    if (search_results["status"] != "ok"):
+        raise Exception("Status for search returned not ok")
+    elif (len(search_results) == 0):
+        raise Exception("Search result return no results")
+    return search_results["result"]["results"]
+
+def search_for_tracks_by_artist(search_query):
+    rdio = Rdio(("ucvxaju3gq3natuvp9w6uxrv", "vwnQkPkDhM"))
+    search_results = rdio.call("getTracksForArtist", {"artist": search_query})
     if (search_results["status"] != "ok"):
         raise Exception("Status for search returned not ok")
     elif (len(search_results) == 0):

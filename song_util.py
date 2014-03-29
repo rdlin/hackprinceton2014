75f90e1800adb24efa757fdbd83fbe23b3285e23
rdlin <rdlin@gmail.com>
2014-03-29 04:07:47 -0400

    Adding cool endpoint that gets a bunch of information for a song :D

--- song_util.py
+++ song_util.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 def get_names_string():
     return "Hello World"
 
-@app.route("/<track_name>/<item>")
-def get_item_for_track_name(track_name=None, item=None):
+@app.route("/search/<track_name>/<item>")
+def get_item_for_track_name_search(track_name=None, item=None):
     return "<br>".join([track[item] for track in search_for_tracks(track_name)])
 
 @app.route("/<item>")
@@ -23,11 +23,27 @@ def get_item_string_for_top_chart_tracks(item=None):
             "consult http://www.rdio.com/developers/docs/web-service/types/track/ for proper types")
     return "<br>".join(get_top_chart_tracks_items(item, top_chart_tracks))
 
+#getting lyrics and a bunch of other information for a track
+@app.route("/search/<track_name>/info")
+def get_info_for_track_name_search(track_name=None):
+    displayString = ""
+    for track in search_for_tracks(track_name):
+        displayString+="Name: " + track["name"]+"<br>"
+        displayString+="Artist: " + track["artist"]+"<br>"
+        displayString+="Lyrics: " + get_lyrics_for_track_name(name=track["name"], artist=track["artist"])+"<br>"
+        displayString+="<br><br>"
+    return displayString
+
 #musixmatch lyric search
 
-def get_lyrics_for_song_name(name='', artist='', lyrics=''):
+def get_lyrics_for_track_name(name='', artist='', lyrics=''):
     track_result_list = track.search(q_track=name, q_artist=artist, q_lyrics=lyrics)
-    return track_result_list[0].lyrics()['lyrics_body']
+    result = filterLyrics(track_result_list[0].lyrics()['lyrics_body'])
+    return result
+
+def filterLyrics(lyrics):
+    lyrics = lyrics.replace("******* This Lyrics is NOT for Commercial use *******", "")
+    return lyrics
 
 #Rdio api calls and stuff
 

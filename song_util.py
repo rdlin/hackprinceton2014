34b3d5c1e8d88b7fd0147a907b9e77225367a456
rdlin <rdlin@gmail.com>
2014-03-29 01:51:02 -0400

    Adding method to get properties of songs in the top charts

--- song_util.py
+++ song_util.py
@@ -1,18 +1,28 @@
 from flask import Flask
-import musixmatch
 from musixmatch import track
 import pdb
-import os
+from rdio import Rdio
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return get_lyric_for_song_name('Stronger', 'Kanye')
+    return ', '.join(get_property_from_top_chart("icon"))
 
-def get_lyric_for_song_name(name='', artist='', lyrics=''):
+def get_lyrics_for_song_name(name='', artist='', lyrics=''):
     track_result_list = track.search(q_track=name, q_artist=artist, q_lyrics=lyrics)
     return track_result_list[0].lyrics()['lyrics_body']
 
+def get_property_from_top_chart(property):
+    #pdb.set_trace()
+    rdio = Rdio(("ucvxaju3gq3natuvp9w6uxrv", "vwnQkPkDhM"))
+    topChartsRequest = rdio.call("getTopCharts", {"type": "Track"})
+    if (topChartsRequest["status"] != "ok"):
+        raise Exception("Status returned not ok")
+    top_items = []
+    for top_item in topChartsRequest["result"]:
+        top_items.append(top_item[property])
+    return top_items
 
 if __name__ == "__main__":
     app.run()

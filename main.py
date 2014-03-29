07c8a1d42340bc500b95bb88d0fae70b92443247
ponyta <chunli.developer@gmail.com>
2014-03-28 23:10:15 -0400

    made background img

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        print 'hello world'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
